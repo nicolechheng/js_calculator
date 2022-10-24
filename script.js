311cff6040e2bcd663baf6f1206557c3ea318c65
nikki <nicolechheng@gmail.com>
2022-10-23 17:20:38 -0700

    implemented calculator functionality

--- script.js
+++ script.js
@@ -12,23 +12,81 @@ class Calculator {
     }
 
     delete() {
-
+        this.currOperand = this.currOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-        this.currOperand = number
+        if (number === '.' && this.currOperand.includes('.'))
+            return
+        this.currOperand = this.currOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-
+        if (this.currOperand === '')
+            return
+        if(this.prevOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.prevOperand = this.currOperand
+        this.currOperand = ''
     }
 
     compute() {
-    
+        let computation
+        const prev = parseFloat(this.prevOperand)
+        const curr = parseFloat(this.currOperand)
+        if (isNaN(prev) || isNaN(curr))
+            return
+        switch (this.operation) {
+            case '+':
+                computation = prev + curr
+                break
+            case '-':
+                computation = prev - curr
+                break
+            case '%':
+                computation = prev / curr
+                break
+            case '*':
+                computation = prev * curr
+                break
+            default:
+                return
+        }
+        this.currOperand = computation
+        this.operation = undefined
+        this.prevOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerNumber = parseFloat(stringNumber.split('.')[0])
+        const decimalNumber = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerNumber)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerNumber.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalNumber != null) {
+            return `${integerDisplay}.${decimalNumber}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
-        this.currOperandTextElement.innerText = this.currOperand
+        this.currOperandTextElement.innerText = this.getDisplayNumber(this.currOperand)
+
+        if (this.operation != null) {
+            this.prevOperandTextElement.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`
+        } else {
+            this.prevOperandTextElement.innerText = ''
+        }
+
     }
 
 }
@@ -36,8 +94,8 @@ class Calculator {
 const numbersButtons = document.querySelectorAll("[data-number]")
 const operationButtons = document.querySelectorAll("[data-operation")
 const equalsButton = document.querySelector("[data-equals]")
-const deleteButton = document.querySelector("[data-equals]")
-const allClearButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
 const prevOperandTextElement = document.querySelector("[data-prev-operand]")
 const currOperandTextElement = document.querySelector("[data-curr-operand]")
 
@@ -49,3 +107,26 @@ numbersButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
